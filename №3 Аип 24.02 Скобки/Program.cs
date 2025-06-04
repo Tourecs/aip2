6d5139061dfe8d8bc5f8f44b4356907f92bd917c
Tourecs <sverlov2006@mail.ru>
2025-06-04 20:14:54 +0600

    Add files via upload

---
+++ №3 Аип 24.02 Скобки/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Строка пуста");
+            return;
+        }
+
+        Stack<char> bracketsStack = new Stack<char>();
+        foreach (char currentChar in input)
+        {
+            if (currentChar == '(' || currentChar == '[' || currentChar == '{')
+            {
+                bracketsStack.Push(currentChar);
+            }
+            else if (currentChar == ')' || currentChar == ']' || currentChar == '}')
+            {
+                if (bracketsStack.Count == 0)
+                {
+                    Console.WriteLine("Скобки расставлены неправильно");
+                    return;
+                }
+
+                char lastOpenBracket = bracketsStack.Pop();
+                if (!IsMatchingBracket(lastOpenBracket, currentChar))
+                {
+                    Console.WriteLine("Скобки расставлены неправильно");
+                    return;
+                }
+            }
+        }
+
+        Console.WriteLine(bracketsStack.Count == 0 ? "Скобки расставлены правильно" : "Скобки расставлены неправильно");
+    }
+
+    static bool IsMatchingBracket(char open, char close)
+    {
+        return (open == '(' && close == ')') ||
+               (open == '[' && close == ']') ||
+               (open == '{' && close == '}');
+    }
+}
