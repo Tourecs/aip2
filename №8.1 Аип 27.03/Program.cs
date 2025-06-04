0935fb5cb64a2f5a780d6b4ef52e6b47feba8acd
Tourecs <sverlov2006@mail.ru>
2025-06-04 20:17:39 +0600

    Add files via upload

---
+++ №8.1 Аип 27.03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<int, int, int> sum = (a, b) => a + b;
+        Func<int, int, int> subtract = (a, b) => a - b;
+        Func<int, int, int> multiply = (a, b) => a * b;
+        Func<int, int, double> divide = (a, b) =>
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль (ошибка)");
+            }
+            return (double)a / b;
+        };
+        Console.WriteLine("Результат суммы: " + sum(5, 3)); // 8
+        Console.WriteLine("Результат азности: " + subtract(5, 3)); // 2
+        Console.WriteLine("Результат произведения: " + multiply(5, 3)); // 15
+        
+        try
+        {
+            Console.WriteLine("Результат деления: " + divide(5, 0)); // Искл
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        Console.WriteLine("Результат деления: " + divide(10, 2)); // 5
+    }
+}
---
+++ №8.2 Аип 27.03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> words = new List<string> { "dog", "monkey", "tiger", "mouse", "cat" };
+        var wordsStartingWithM = words.Where(word => word.StartsWith("m"));
+
+        foreach (var word in wordsStartingWithM)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
