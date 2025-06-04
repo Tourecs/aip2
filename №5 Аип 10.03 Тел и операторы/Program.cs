4a62c16e952b7ab6451d3cfb5e83621b8b125757
Tourecs <sverlov2006@mail.ru>
2025-06-04 20:16:39 +0600

    Add files via upload

---
+++ №5 Аип 10.03 Тел и операторы/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+public class Phone
+{
+    public string PhoneNumber;
+
+    public Phone(string phoneNumber)
+    {
+        if (IsValidPhoneNumber(phoneNumber))
+        {
+            PhoneNumber = phoneNumber;
+        }
+        else
+        {
+            throw new ArgumentException("Неверный номер телефона.");
+        }
+    }
+
+    private bool IsValidPhoneNumber(string number)
+    {
+        // Логика проверки номера телефона
+        return !string.IsNullOrWhiteSpace(number) && number.All(char.IsDigit);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Phone myPhone = new Phone("79999999999");
+            Console.WriteLine($"Номер телефона: {myPhone.PhoneNumber}");
+            myPhone.PhoneNumber = "abc";
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
